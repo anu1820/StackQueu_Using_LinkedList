214e7b67c38aaab7c77b6a70f88cec293c583466
anu1820 <anurajpatil35@gmail.com>
2023-01-14 01:28:44 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace StackQueueusinglinkedlist
+﻿using StackQueueUsingLinkedList;
+
+namespace StackQueueusinglinkedlist
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine(" Welocome to Stack & Queue Programs Using Linked list");
+            Stack obj = new Stack();
+            Console.WriteLine("After push the elements are : ");
+
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            Console.WriteLine();
+
+            Console.WriteLine("Top Element of the stack is : " + obj.peek());
+
 
         }
     }
