e65aff39cb91ebee7a9a746b36da15dc75acefba
anu1820 <anurajpatil35@gmail.com>
2023-01-14 01:10:41 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StackQueueusinglinkedlist
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welocome to Stack & Queue Programs Using Linked list");
+
+        }
+    }
+}
