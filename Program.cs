35869108e001e28ee3d456aef1cfaf2e1aa93305
anu1820 <anurajpatil35@gmail.com>
2023-01-14 01:41:20 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace StackQueueusinglinkedlist
             obj.push(30);
             obj.push(56);
             Console.WriteLine();
+            Console.WriteLine("Item popped from stack = " + obj.pop());
 
             Console.WriteLine("Top Element of the stack is : " + obj.peek());
 
